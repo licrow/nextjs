7ee726c8d45d058d45452348f018a2fb4ac4c2a4
licrow <39090180+licrow@users.noreply.github.com>
2021-11-16 22:16:34 +0000

    Update domain.js

--- pages/domain.js
+++ pages/domain.js
@@ -15,17 +15,17 @@ export default function Home() {
           Favourite and least favourite things
         </h1>
 		<div>
-			<p>Hi there,<br />
-Thanks for contacting Vercel Support.<br />
-You can add a custom domain to your Vercel project. Different methods are available dependent on your use case. This is fully documented here:</p> <a href=https://vercel.com/docs/concepts/projects/custom-domains>Custom Domains</a><br />
+			<p>Hi there,</p>
+<p>Thanks for contacting Vercel Support.</p>
+<p>You can add a custom domain to your Vercel project. Different methods are available dependent on your use case. This is fully documented here:</p> <a href=https://vercel.com/docs/concepts/projects/custom-domains>Custom Domains</a><br />
 <p>The main two methods are as follows:<br />
 1.	A/CNAME Proxying: You can create DNS records pointing to Vercel at your existing authoritative nameservers. We will provide you with the correct records which will be automatically validated after DNS propagation. The type of record will depend on the domain being added:<br />
 a.	Root domains will require an A record, as nameservers do not typically support CNAME’s on the root domain.<br />
 b.	Subdomains can be added using a CNAME record, which allows for the IP address to be pulled from the Vercel hostname dynamically.<br />
-2.	Change of Nameservers: You can amend your authoritative nameservers to those provided by Vercel. Once propagated, these will activate and management of DNS records can be handled through Vercel directly.<br />
-Once added, you can assign the domain in question to either your default branch or to a different branch to allow for separation between production/development environments and so on.<br />
-I will set this ticket to pending for now but please let us know if you require any further assistance and we will be happy to help.<br />
-Kind regards,<br />
+2.	Change of Nameservers: You can amend your authoritative nameservers to those provided by Vercel. Once propagated, these will activate and management of DNS records can be handled through Vercel directly.</p>
+<p>Once added, you can assign the domain in question to either your default branch or to a different branch to allow for separation between production/development environments and so on.</p>
+<p>I will set this ticket to pending for now but please let us know if you require any further assistance and we will be happy to help.</p>
+<p>Kind regards,<br />
 Liam Crowter<br />
 Vercel Support<br /></p>
 
