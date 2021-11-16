d73634f1265acc57d5c7b34004a0fcd3163da9f6
licrow <39090180+licrow@users.noreply.github.com>
2021-11-16 19:36:40 +0000

    Update redirect.js

--- pages/redirect.js
+++ pages/redirect.js
@@ -16,7 +16,7 @@ return (
       <div>
          <p>Hi there, Thanks for contacting Vercel Support. You can redirect within the application by amending the configuration file for your project. This differs dependent on the type of project you are using. If your project is using Next.js, you can add the following code to your next.config.js file to carry out a redirect from one path to another:</p>
          <p className={styles.code}>
-            module.exports = {
+            'module.exports = {
             async redirects() {
             return [
             {
@@ -26,11 +26,11 @@ return (
             },
             ]
             },
-            }
+            }'
          </p>
          <p>When using Next.js, if you wish to use wildcards in your redirection to include anything within the /about URI path, you can do this by including :path* within the source for the path in question, like so:</p>
          <p className={styles.code}>
-            module.exports = {
+            'module.exports = {
             async redirects() {
             return [
             {
@@ -40,11 +40,11 @@ return (
             },
             ]
             },
-            }
+            }'
          </p>
          <p>If you are not using Next.js, you can redirect within the vercel.json file using the following code:</p>
          <p className={styles.code}>
-            {
+            '{
             "redirects": [
             { 
             "source": "/view-source",
@@ -52,7 +52,7 @@ return (
             "permanent": true
             }
             ]
-            }
+            }'
          </p>
          <p>You can also amend the type of redirect (whether permanent or temporary, or use a specific status code in response). You may find the following documents useful in providing more information for the use case you have here: https://vercel.com/support/articles/does-vercel-support-permanent-redirects https://nextjs.org/docs/api-reference/next.config.js/redirects I will set this ticket to pending for now but please let us know if you require any further assistance and we will be happy to help. Kind regards, Liam Crowter Vercel Support</p>
       </div>
