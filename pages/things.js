cec31298d71b9c0c0e94b6b1db4651ec8769960a
licrow <39090180+licrow@users.noreply.github.com>
2021-11-16 19:17:12 +0000

    Update things.js

--- pages/things.js
+++ pages/things.js
@@ -1,5 +1,10 @@
-import Head from 'next/head' import Image from 'next/image' import styles from '../styles/Home.module.css' export default function Home() { return (
-<div className={styles.container}>
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+	  <div className={styles.container}>
 
 	<Head>
 		<title>Liam&#39;s Project Site</title>
