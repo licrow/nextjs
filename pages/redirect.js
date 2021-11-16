3c14093f8dede3d0f0a24e4b8999455e8a7f0fbf
licrow <39090180+licrow@users.noreply.github.com>
2021-11-16 20:46:24 +0000

    Update redirect.js

--- pages/redirect.js
+++ pages/redirect.js
@@ -22,7 +22,7 @@ export default function Home() {
 	 
 	 <p>You can redirect within the application by amending the configuration file for your project. This differs dependent on the type of project you are using. If your project is using Next.js, you can add the following code to your next.config.js file to carry out a redirect from one path to another:</p>
 
-	 <code className={styles.code}></code>
+	 <code className={styles.code}>"module.exports = {"<br />"  async redirects() {"<br />"    return ["<br />"      {"</code>
 
          <p>When using Next.js, if you wish to use wildcards in your redirection to include anything within the /about URI path, you can do this by including :path* within the source for the path in question, like so:</p>
 
