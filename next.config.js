b2f1d4451618c4e464a852319aa1386570f12981
licrow <39090180+licrow@users.noreply.github.com>
2022-05-16 16:38:12 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,14 +3,10 @@ module.exports = {
   
   async rewrites() {
     return [
-      {
-        beforeFiles: [
           {
             source: '/:path*',
             destination: 'https://example.example.com',
             has: [{ type: 'host', value: 'example.evangelionunit.one' }],
-          },
-        ],
       },
     ]
   },
