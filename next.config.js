51e11ec3693cd9cd737bc4a4ca56ab525b24c7bb
licrow <39090180+licrow@users.noreply.github.com>
2022-05-16 15:58:27 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,23 @@
 module.exports = {
   reactStrictMode: true,
+
+async rewrites() {
+  return [
+  {
+  "rewrites": [
+    {
+      "source": "/:path*",
+      "has": [
+        {
+          "type": "host",
+          "value": "example.evangelionunit.one"
+        }
+      ],
+        destination: 'https://example.example.com'
+      },
+    ]
+  },
+}
   
   async headers() {
     return [
