d33b94f7f1b8b7413880abc7497606ab5d53ca78
licrow <39090180+licrow@users.noreply.github.com>
2022-06-30 09:55:34 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,14 @@ module.exports = {
             source: '/examplepath',
             destination: 'https://example.com',
             has: [{ type: 'host', value: 'www.evangelionunit.one' }],
+          headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=1, stale-while-revalidate=59",
+          },
       },
     ]
   },
 }
+
+module.exports = nextConfig;
